5c6c7f48d7c5c223986e095ab231d89450160e94
KID <50666207+KID421@users.noreply.github.com>
2020-04-29 16:05:16 +0800

    完成

---
+++ UnityGAS/Assets/UnityGAS.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class UnityGAS : MonoBehaviour
+{
+    [Header("顯示文字")]
+    public Text textShow;
+
+    public void GetSheet()
+    {
+        StartCoroutine(StartGetSheet());
+    }
+
+    public void SetSheet()
+    {
+        StartCoroutine(StartSetSheet());
+    }
+
+    private IEnumerator StartGetSheet()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("method", "讀取");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://script.google.com/macros/s/AKfycbzuQPbyoQBbI2QLLCXc-dnpHupnkXLEXQzoCSWZvgSlpfGawwTI/exec", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                print("錯誤：" + www.error);
+            }
+            else
+            {
+                textShow.text = www.downloadHandler.text;
+                textShow.text += "\n讀取資料成功";
+            }
+        }
+    }
+
+    private IEnumerator StartSetSheet()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("method", "寫入");
+        form.AddField("欄位名稱", "欄位資料");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://script.google.com/macros/s/AKfycbzuQPbyoQBbI2QLLCXc-dnpHupnkXLEXQzoCSWZvgSlpfGawwTI/exec", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                print("錯誤：" + www.error);
+            }
+            else
+            {
+                textShow.text = www.downloadHandler.text;
+                textShow.text += "\n讀取資料成功";
+            }
+        }
+    }
+}
---
+++ UnityGAS_Tutorial/Assets/UnityGAS.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class UnityGAS : MonoBehaviour
+{
+    private IEnumerator StartGetSheet()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("method", "讀取");
+        form.AddField("row", 2);
+        form.AddField("col", 3);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://script.google.com/macros/s/AKfycbw6J4AAJf-DRi-Rs_yNGRTCBs1sJetlxXxUFuwcrTNSbWh5n04/exec", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                print("錯誤：" + www.error);
+            }
+            else
+            {
+                print(www.downloadHandler.text);
+            }
+        }
+    }
+
+    private IEnumerator StartSetSheet()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("method", "寫入");
+        form.AddField("name", "飛龍");
+        form.AddField("hp", 750);
+        form.AddField("atk", 250);
+        form.AddField("speed", "5.5");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://script.google.com/macros/s/AKfycbw6J4AAJf-DRi-Rs_yNGRTCBs1sJetlxXxUFuwcrTNSbWh5n04/exec", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                print("錯誤：" + www.error);
+            }
+            else
+            {
+                print(www.downloadHandler.text);
+            }
+        }
+    }
+
+    private IEnumerator StartChangeSheet()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("method", "修改");
+        form.AddField("row", 4);
+        form.AddField("col", 1);
+        form.AddField("name", "測試");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://script.google.com/macros/s/AKfycbw6J4AAJf-DRi-Rs_yNGRTCBs1sJetlxXxUFuwcrTNSbWh5n04/exec", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                print("錯誤：" + www.error);
+            }
+            else
+            {
+                print(www.downloadHandler.text);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartGetSheet());
+        StartCoroutine(StartSetSheet());
+        StartCoroutine(StartChangeSheet());
+    }
+}
